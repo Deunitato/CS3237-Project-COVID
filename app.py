d9ffb61bba50f7b19bfcdba87ae480284e184884
deunitato <charlotte.limwt@gmail.com>
2020-10-18 00:54:14 +0800

    Add skills visibility

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 
-
+SKILLS = {"Slash" : True, "Shield": True}
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('monster_attack.html')
+    print(SKILLS)
+    return render_template('monster_attack.html', Skills = SKILLS)
 
     
 if __name__ == "__main__":
